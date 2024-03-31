c25f3f9df7815e12dd8469e65e689e00ba5f6f56
llltttwww <2685287173@qq.com>
2024-03-31 16:17:15 +0800

    complete version with recorder

--- widget.cpp
+++ widget.cpp
@@ -8,28 +8,33 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    ui->btnStop->setEnabled(false);
-    connect(ui->btnStart,SIGNAL(clicked()),this,SLOT(do_actStart_clicked()));
+    ui->btnStopTransfer->setEnabled(false);
+    connect(ui->btnStartTransfer,SIGNAL(clicked()),this,SLOT(do_actStart_clicked()));
     thread=new transThread;
     this->setWindowIcon(QIcon("..\\DirectTransfer\\resources\\image.jpg"));
     // connect(this,&Widget::startran,[=](){ui->textResult->setPlainText("转换中，请等待"); });
-    connect(this,&Widget::startran,[=](){QMetaObject::invokeMethod(ui->textResult,"setPlainText",Qt::QueuedConnection,Q_ARG(QString,tr("转换中，请等待")));ui->btnStart->setEnabled(false);ui->btnChooseFile->setEnabled(false);ui->btnSave->setEnabled(false);ui->filePathEdit->setEnabled(false);ui->textResult->setEnabled(false);ui->btnStop->setEnabled(true);});
+    QAudioFormat format;
+    format.setSampleRate(8000);
+    format.setChannelCount(1);
+    format.setSampleFormat(QAudioFormat::Int16);
+    audio=new QAudioSource(format,this);
+    connect(this,&Widget::startran,[=](){QMetaObject::invokeMethod(ui->textResult,"setPlainText",Qt::QueuedConnection,Q_ARG(QString,tr("转换中，请等待")));ui->btnStartTransfer->setEnabled(false);ui->btnChooseFile->setEnabled(false);ui->btnSave->setEnabled(false);ui->filePathEdit->setEnabled(false);ui->textResult->setEnabled(false);ui->btnStopTransfer->setEnabled(true);});
     connect(this,&Widget::startran,[=](){thread->rcv_str(ui->filePathEdit->text().toStdString());thread->start();});
     // connect(thread,&transThread::send_text,[=](std::string text){qDebug()<<"textrcvd\n";ui->textResult->setPlainText(QString::fromLocal8Bit(text));});
-    connect(thread,&transThread::send_text,[=](std::string text){QMetaObject::invokeMethod(ui->textResult,"setPlainText",Qt::QueuedConnection,QString::fromLocal8Bit(text));QMetaObject::invokeMethod(ui->btnStart,"setEnabled",Qt::QueuedConnection,true);
+    connect(thread,&transThread::send_text,[=](std::string text){QMetaObject::invokeMethod(ui->textResult,"setPlainText",Qt::QueuedConnection,QString::fromLocal8Bit(text));QMetaObject::invokeMethod(ui->btnStartTransfer,"setEnabled",Qt::QueuedConnection,true);
                         QMetaObject::invokeMethod(ui->btnChooseFile,"setEnabled",Qt::QueuedConnection,true);
 QMetaObject::invokeMethod(ui->btnSave,"setEnabled",Qt::QueuedConnection,true);
                         QMetaObject::invokeMethod(ui->filePathEdit,"setEnabled",Qt::QueuedConnection,true);
 QMetaObject::invokeMethod(ui->textResult,"setEnabled",Qt::QueuedConnection,true);
-    QMetaObject::invokeMethod(ui->btnStop,"setEnabled",Qt::QueuedConnection,false);});
+    QMetaObject::invokeMethod(ui->btnStopTransfer,"setEnabled",Qt::QueuedConnection,false);});
     connect(this,&Widget::threadstop,[=](){QMetaObject::invokeMethod(ui->textResult,"setPlainText",Qt::QueuedConnection,Q_ARG(QString,tr("转换已中止")));
-QMetaObject::invokeMethod(ui->btnStart,"setEnabled",Qt::QueuedConnection,true);
+QMetaObject::invokeMethod(ui->btnStartTransfer,"setEnabled",Qt::QueuedConnection,true);
                         QMetaObject::invokeMethod(ui->btnChooseFile,"setEnabled",Qt::QueuedConnection,true);
 QMetaObject::invokeMethod(ui->btnSave,"setEnabled",Qt::QueuedConnection,true);
                         QMetaObject::invokeMethod(ui->filePathEdit,"setEnabled",Qt::QueuedConnection,true);
 QMetaObject::invokeMethod(ui->textResult,"setEnabled",Qt::QueuedConnection,true);
-    QMetaObject::invokeMethod(ui->btnStop,"setEnabled",Qt::QueuedConnection,false);});
-
+    QMetaObject::invokeMethod(ui->btnStopTransfer,"setEnabled",Qt::QueuedConnection,false);});
+    connect(this->audio,&QAudioSource::stateChanged,this,&Widget::onStateChanged);
 }
 Widget::~Widget()
 {
@@ -37,7 +42,7 @@ Widget::~Widget()
 }
 
 
-void Widget::on_btnStart_clicked()
+void Widget::on_btnStartTransfer_clicked()
 {
     // ui->textResult->setPlainText("Waiting");
     // qDebug()<<"clicked\n";
@@ -83,14 +88,145 @@ void Widget::on_btnSave_clicked()
 
 }
 
-void Widget::on_btnStop_clicked()
+void Widget::on_btnStopTransfer_clicked()
 {
     qDebug()<<"stop\n";
     thread->terminate();
-    ui->btnStart->setEnabled(true);
+    ui->btnStartTransfer->setEnabled(true);
     ui->btnChooseFile->setEnabled(true);
-    ui->btnStop->setEnabled(true);
+    ui->btnStopTransfer->setEnabled(true);
     ui->filePathEdit->setEnabled(true);
     ui->textResult->setEnabled(true);
     emit(threadstop());
 }
+
+
+
+void Widget::on_btnStartRecord_clicked()
+{
+    qDebug()<<"startRecord\n";
+    QString pos=ui->dirPathEdit->text();
+    QString fname=ui->fileNameEdit->text();
+    pos.append("/");
+    pos.append(fname);
+    pos.append(".raw");
+    destinationFile =new QFile(pos);
+    destinationFile->open(QIODevice::WriteOnly);
+
+    audio->start(destinationFile);
+}
+
+
+void Widget::on_btnStopRecord_clicked()
+{
+
+    qDebug()<<"stopRecord\n";
+    QString pos=ui->dirPathEdit->text();
+    QString fname=ui->fileNameEdit->text();
+    QString raw=pos+"/"+fname+".raw",wav=pos+"/"+fname+".wav";
+
+    audio->stop();
+    this->addWavHeader(raw,wav);
+    destinationFile->close();
+    ui->filePathEdit->setText(wav);
+}
+
+qint64 Widget::addWavHeader(QString catheFileName , QString wavFileName)
+{
+    // 开始设置WAV的文件头
+    // 这里具体的数据代表什么含义请看上一篇文章（Qt之WAV文件解析）中对wav文件头的介绍
+    WAVFILEHEADER WavFileHeader;
+    qstrcpy(WavFileHeader.RiffName, "RIFF");
+    qstrcpy(WavFileHeader.WavName, "WAVE");
+    qstrcpy(WavFileHeader.FmtName, "fmt ");
+    qstrcpy(WavFileHeader.DATANAME, "data");
+
+    // 表示 FMT块 的长度
+    WavFileHeader.nFmtLength = 16;
+    // 表示 按照PCM 编码;
+    WavFileHeader.nAudioFormat = 1;
+    // 声道数目;
+    WavFileHeader.nChannleNumber = 1;
+    // 采样频率;
+    WavFileHeader.nSampleRate = 8000;
+
+    // nBytesPerSample 和 nBytesPerSecond这两个值通过设置的参数计算得到;
+    // 数据块对齐单位(每个采样需要的字节数 = 通道数 × 每次采样得到的样本数据位数 / 8 )
+    WavFileHeader.nBytesPerSample = 2;
+    // 波形数据传输速率
+    // (每秒平均字节数 = 采样频率 × 通道数 × 每次采样得到的样本数据位数 / 8  = 采样频率 × 每个采样需要的字节数 )
+    WavFileHeader.nBytesPerSecond = 16000;
+
+    // 每次采样得到的样本数据位数;
+    WavFileHeader.nBitsPerSample = 16;
+
+    QFile cacheFile(catheFileName);
+    QFile wavFile(wavFileName);
+
+    if (!cacheFile.open(QIODevice::ReadWrite))
+    {
+        return -1;
+    }
+    if (!wavFile.open(QIODevice::WriteOnly))
+    {
+        return -2;
+    }
+
+    int nSize = sizeof(WavFileHeader);
+    qint64 nFileLen = cacheFile.bytesAvailable();
+
+    WavFileHeader.nRiffLength = nFileLen - 8 + nSize;
+    WavFileHeader.nDataLength = nFileLen;
+
+    // 先将wav文件头信息写入，再将音频数据写入;
+    wavFile.write((char *)&WavFileHeader, nSize);
+    wavFile.write(cacheFile.readAll());
+
+    cacheFile.close();
+    wavFile.close();
+
+    return nFileLen;
+}
+
+void Widget::on_btnChooseDir_clicked()
+{
+    QFileDialog fileDialog= QFileDialog(this);
+    fileDialog.setFileMode(QFileDialog::Directory);
+    QString selectDir = QFileDialog::getExistingDirectory();
+    qDebug()<<selectDir<<'\n';
+    ui->dirPathEdit->setText(selectDir);
+}
+
+void Widget::on_btnPauseRecord_clicked()
+{
+
+    qDebug()<<"pauseRecord\n";
+    audio->suspend();
+}
+
+void Widget::onStateChanged(QAudio::State state)
+{
+    switch (state) {
+    case QAudio::State::ActiveState:
+        ui->btnStartRecord->setEnabled(false);
+        ui->btnPauseRecord->setEnabled(true);
+        ui->btnStopRecord->setEnabled(true);
+        ui->btnPauseRecord->setText("暂停录音");
+        break;
+    case QAudio::State::SuspendedState:
+        ui->btnPauseRecord->setEnabled(true);
+        ui->btnStopRecord->setEnabled(true);
+        ui->btnPauseRecord->setText("继续录音");
+        break;
+    case QAudio::State::StoppedState:
+        ui->btnPauseRecord->setText("暂停录音");
+        ui->btnStartRecord->setEnabled(true);
+        ui->btnPauseRecord->setEnabled(false);
+        ui->btnStopRecord->setEnabled(false);
+        break;
+    }
+
+}
+
+
+
--- widget.h
+++ widget.h
@@ -6,6 +6,34 @@
 #include <QFileDialog>
 #include "sub_thread.h"
 #include <QMessageBox>
+#include <QtMultimedia/QAudioSource>
+
+struct WAVFILEHEADER
+{
+    // RIFF 头
+    char RiffName[4];
+    unsigned long nRiffLength;
+
+    // 数据类型标识符
+    char WavName[4];
+
+    // 格式块中的块头
+    char FmtName[4];
+    unsigned long nFmtLength;
+
+    // 格式块中的块数据
+    unsigned short nAudioFormat;
+    unsigned short nChannleNumber;
+    unsigned long nSampleRate;
+    unsigned long nBytesPerSecond;
+    unsigned short nBytesPerSample;
+    unsigned short nBitsPerSample;
+
+    // 数据块中的块头
+    char    DATANAME[4];
+    unsigned long   nDataLength;
+};
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,14 +46,28 @@ class Widget : public QWidget
     Q_OBJECT
 
 public:
+    QAudioSource* audio;
+    QFile* destinationFile;
+    qint64 addWavHeader(QString catheFileName , QString wavFileName);
+    void onStateChanged(QAudio::State state);
+
+public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
 private slots:
-    void on_btnStart_clicked();
+    void on_btnStartTransfer_clicked();
     void on_btnChooseFile_clicked();
     void on_btnSave_clicked();
-    void on_btnStop_clicked();
+    void on_btnStopTransfer_clicked();
+
+    void on_btnStartRecord_clicked();
+
+    void on_btnStopRecord_clicked();
+
+    void on_btnChooseDir_clicked();
+
+    void on_btnPauseRecord_clicked();
 
 private:
 
